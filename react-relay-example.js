59bf9f7c92dbbc27c04b403699555e16f1698e85
antoine_cellier <antoine.cellier@zenika.com>
2016-03-07 20:10:49 +0100

    add react-relay example

---
+++ react-relay-example.js
@@ -0,0 +1,47 @@
+/* Connexion avec le serveur GraphQL*/
+Relay.injectNetworkLayer(
+  new Relay.DefaultNetworkLayer('http://localhost:8080/graphql')
+);
+
+/* Requête GraphQL */
+const query = {
+  name: 'AppQueries',
+  queries: {
+    Post: () => Relay.QL`query{ user }`,
+  },
+};
+
+/* Conteneur Relay du composant */
+const componentContainer = Relay.createContainer(App, {
+  fragments: {
+    posts: () => Relay.QL`
+      fragment on Post {
+        title
+        category
+      }
+    `,
+  },
+});
+
+/* Déclaration du composant */
+class App extends React.Component {
+  render() {
+    console.log(this.props.appuisVelos);
+    return (
+      <div>
+        <ul>
+          {this.props.posts(p =>
+            <h1>{p.title}</h1>
+            <i>{p.category}</i>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+/* Conteneur Relay de l'application */
+ReactDOM.render(
+  <Relay.RootContainer Component={componentContainer} route={query} />,
+  document.getElementById('root')
+)
